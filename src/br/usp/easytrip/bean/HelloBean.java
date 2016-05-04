7a40469a64a75e3d39aa451dea4a3f0f8af0f56c
LucasDelboni <lucas_cd460@hotmail.com>
2016-05-04 15:36:14 -0300

    colocado o index.jsp usando o EJB

--- src/br/usp/easytrip/bean/HelloBean.java
+++ src/br/usp/easytrip/bean/HelloBean.java
@@ -1,7 +1,13 @@
 package br.usp.easytrip.bean;
 
+import java.rmi.RemoteException;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import com.cdyne.ws.WeatherWS.WeatherSoapProxy;
+
+import NET.webserviceX.www.ConvertTemperatureSoapProxy;
 
 /**
  * Session Bean implementation class HelloBean
@@ -16,6 +22,19 @@ public class HelloBean {
      */
     public String from(){
     	return "EJB";
+    }
+    
+    public String getTemperaturaZipCode(String zipCode) throws RemoteException{
+    	WeatherSoapProxy wc = new WeatherSoapProxy();
+		String temperaturaZipCode = wc.getCityForecastByZIP(zipCode).getForecastResult()[0].getTemperatures().getMorningLow();
+		return temperaturaZipCode;
+    }
+    
+    public double fahrenheitToCelsius(String temperaturaFahrenheit) throws RemoteException{
+    	ConvertTemperatureSoapProxy cc = new ConvertTemperatureSoapProxy();
+	    double fahrenheit = Double.parseDouble(temperaturaFahrenheit);
+	    double temperaturaCelsius = cc.convertTemp(fahrenheit,"degreeFahrenheit" , "degreeCelsius");
+		return temperaturaCelsius;
     }
 
 
